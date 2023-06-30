978908807c1f47d4ec80de29fe0b9e70c2b61bf8
BASTIEN ESQUIROS <bastien.esquiros@hotmail.com>
2023-06-30 11:32:10 +0200

    init: first commit

---
+++ src/main/java/edu/datascientest/library_project/LibraryProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.datascientest.library_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/datascientest/library_project/LibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.datascientest.library_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
