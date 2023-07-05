b9c5c61028a16a7b09cd778408536458bb44b877
BASTIEN ESQUIROS <bastien.esquiros@hotmail.com>
2023-07-05 13:09:30 +0200

    *

--- backend/src/main/java/edu/datascientest/library_project/ouvrage/Ouvrage.java
+++ backend/src/main/java/edu/datascientest/library_project/ouvrage/Ouvrage.java
@@ -1,10 +1,5 @@
 package edu.datascientest.library_project.ouvrage;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
@@ -14,111 +9,129 @@ import edu.datascientest.library_project.exemplaire.Exemplaire;
 import edu.datascientest.library_project.type_ouvrage.TypeOuvrage;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Entity
-@Table(name="ouvrage")
+@Table(name = "ouvrage")
 public class Ouvrage {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(nullable=false)
-	private Integer id_ouvrage;
-	private String titre;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer id_ouvrage;
+    private String titre;
 
-	@ManyToOne (cascade = CascadeType.ALL)
-	@JoinColumn(name = "id_type")
-	@JsonBackReference
-	private TypeOuvrage typeOuvrage;
 
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JsonIgnore
-	@JoinTable(
-		name="ecrire",
-		joinColumns = @JoinColumn(name = "id_ouvrage"),
-		inverseJoinColumns = @JoinColumn(name="id_auteur")
-	)
-	private List<Auteur> auteurs;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_auteur")
+    @JsonBackReference
+    private Auteur auteur;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_type")
+    @JsonBackReference
+    private TypeOuvrage typeOuvrage;
 
-	@ManyToMany(fetch = FetchType.EAGER) @JsonIgnore
-	@JoinTable(
-		name = "reserve",
-		joinColumns = @JoinColumn(name = "id_ouvrage"),
-		inverseJoinColumns = @JoinColumn(name = "id_abonne")
-		)
-	private List<Abonne> abonnes;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JsonIgnore
+    @JoinTable(
+            name = "ecrire",
+            joinColumns = @JoinColumn(name = "id_ouvrage"),
+            inverseJoinColumns = @JoinColumn(name = "id_auteur")
+    )
+    private List<Auteur> auteurs;
 
-	@OneToMany(mappedBy="ouvrage", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	@JsonManagedReference @JsonIgnore
-	private List<Exemplaire> exemplaires;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JsonIgnore
+    @JoinTable(
+            name = "reserve",
+            joinColumns = @JoinColumn(name = "id_ouvrage"),
+            inverseJoinColumns = @JoinColumn(name = "id_abonne")
+    )
+    private List<Abonne> abonnes;
 
-	public Ouvrage() {	}
 
-	public Ouvrage(String titre, TypeOuvrage typeOuvrage, Auteur... auteurs) {
-		this.titre = titre;
-		this.typeOuvrage = typeOuvrage;
-		if(this.auteurs == null) this.auteurs = new ArrayList<>();
-		this.auteurs.addAll(Arrays.stream(auteurs).toList());
-	}
+    @OneToMany(mappedBy = "ouvrage", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonManagedReference
+    @JsonIgnore
+    private List<Exemplaire> exemplaires;
 
-	public Ouvrage(Integer id_ouvrage, String titre, TypeOuvrage typeOuvrage, List<Auteur> auteurs, List<Abonne> abonnes, List<Exemplaire> exemplaires) {
-		this.id_ouvrage = id_ouvrage;
-		this.titre = titre;
-		this.typeOuvrage = typeOuvrage;
-		this.auteurs = auteurs;
-		this.abonnes = abonnes;
-		this.exemplaires = exemplaires;
-	}
+    public Ouvrage() {
+    }
 
-	public Integer getId_ouvrage() {
-		return id_ouvrage;
-	}
-	public void setId_ouvrage(Integer id_ouvrage) {
-		this.id_ouvrage = id_ouvrage;
-	}
-	public String getTitre() {
-		return titre;
-	}
-	public void setTitre(String titre) {
-		this.titre = titre;
-	}
-	public TypeOuvrage getTypeOuvrage() {
-		return typeOuvrage;
-	}
-	public void setTypeOuvrage(TypeOuvrage typeOuvrage) {
-		this.typeOuvrage = typeOuvrage;
-	}
+    public Ouvrage(String titre, TypeOuvrage typeOuvrage, Auteur... auteurs) {
+        this.titre = titre;
+        this.typeOuvrage = typeOuvrage;
+        if (this.auteurs == null) this.auteurs = new ArrayList<>();
+        this.auteurs.addAll(Arrays.stream(auteurs).toList());
+    }
 
-	public List<Auteur> getAuteurs() {
-		return auteurs;
-	}
+    public Ouvrage(Integer id_ouvrage, String titre, TypeOuvrage typeOuvrage, List<Auteur> auteurs, List<Abonne> abonnes, List<Exemplaire> exemplaires) {
+        this.id_ouvrage = id_ouvrage;
+        this.titre = titre;
+        this.typeOuvrage = typeOuvrage;
+        this.auteurs = auteurs;
+        this.abonnes = abonnes;
+        this.exemplaires = exemplaires;
+    }
 
-	public void setAuteurs(List<Auteur> auteurs) {
-		this.auteurs = auteurs;
-	}
+    public Integer getId_ouvrage() {
+        return id_ouvrage;
+    }
 
-	public List<Exemplaire> getExemplaires() {
-		return exemplaires;
-	}
+    public void setId_ouvrage(Integer id_ouvrage) {
+        this.id_ouvrage = id_ouvrage;
+    }
 
-	public void setExemplaires(List<Exemplaire> exemplaires) {
-		this.exemplaires = exemplaires;
-	}
+    public String getTitre() {
+        return titre;
+    }
 
-	public List<Abonne> getAbonnes() {
-		return abonnes;
-	}
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
 
-	public void setAbonnes(List<Abonne> abonnes) {
-		this.abonnes = abonnes;
-	}
+    public TypeOuvrage getTypeOuvrage() {
+        return typeOuvrage;
+    }
 
-	@Override
-	public String toString() {
-		return "Ouvrage{" +
-				"id_ouvrage=" + id_ouvrage +
-				", titre='" + titre + '\'' +
-				", typeOuvrage=" + typeOuvrage +
-				", auteurs=" + auteurs +
-				", abonnes=" + abonnes +
-				", exemplaires=" + exemplaires +
-				'}';
-	}
+    public void setTypeOuvrage(TypeOuvrage typeOuvrage) {
+        this.typeOuvrage = typeOuvrage;
+    }
+
+    public List<Auteur> getAuteurs() {
+        return auteurs;
+    }
+
+    public void setAuteurs(List<Auteur> auteurs) {
+        this.auteurs = auteurs;
+    }
+
+    public List<Exemplaire> getExemplaires() {
+        return exemplaires;
+    }
+
+    public void setExemplaires(List<Exemplaire> exemplaires) {
+        this.exemplaires = exemplaires;
+    }
+
+    public List<Abonne> getAbonnes() {
+        return abonnes;
+    }
+
+    public void setAbonnes(List<Abonne> abonnes) {
+        this.abonnes = abonnes;
+    }
+
+    @Override
+    public String toString() {
+        return "Ouvrage{" +
+                "id_ouvrage=" + id_ouvrage +
+                ", titre='" + titre + '\'' +
+                ", typeOuvrage=" + typeOuvrage +
+                ", auteurs=" + auteurs +
+                ", abonnes=" + abonnes +
+                ", exemplaires=" + exemplaires +
+                '}';
+    }
 }
